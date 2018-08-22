b0ac764c5010f360408ceb996bf1bba8756cae7f
张建春 <zhangjianchun8023@163.com>
2018-08-23 00:54:09 +0800

    增加了线程8锁，改进了生产者消费者模式

--- lombook/src/main/java/com/atguigu/lombook/TestLombok.java
+++ lombook/src/main/java/com/atguigu/lombook/TestLombok.java
@@ -4,6 +4,7 @@ import com.sun.istack.internal.NotNull;
 import lombok.*;
 import lombok.experimental.Accessors;
 
+
 @NoArgsConstructor
 @AllArgsConstructor
 //加了这个注解就所有的都是标配，不加这个可以自定义get/set等！
--- lombook/src/main/java/com/atguigu/producerandconsumer/ProducerAndConsumerDemo.java
+++ lombook/src/main/java/com/atguigu/producerandconsumer/ProducerAndConsumerDemo.java
@@ -1,58 +1,126 @@
 package com.atguigu.producerandconsumer;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 //模拟生产一个消费一个的情形，共进行10轮
 //线程 操纵 资源类 高内聚 低耦合 判断 干活 通知 虚假唤醒用while
 //资源类
 class Cake{
     private int number = 0;
+    private Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
 
-    public synchronized void consumCake() throws InterruptedException {
-        //判断
-        while (number == 0){
-            this.wait();
+
+    /*public synchronized void consumCake() throws InterruptedException {
+       //判断
+       while (number == 0) {
+           this.wait();
+       }
+       //干活
+       --number;
+       System.out.println(Thread.currentThread().getName()+"消费了一块蛋糕！还剩"+number+"块蛋糕！");
+       //通知
+       this.notifyAll();
+   }
+
+  public synchronized void productCake() throws InterruptedException {
+           //判断
+           while (number != 0) {
+               this.wait();
+           }
+           //干活
+           ++number;
+           System.out.println(Thread.currentThread().getName()+"生产了一块蛋糕！还剩"+number+"块蛋糕！");
+           //通知
+           this.notifyAll();
+   }*/
+    //这个为什么不行呢？因为是一套的！
+    //生产的方法
+    public void productCake() throws InterruptedException {
+       lock.lock();
+       try {
+            //判断
+            while ( number != 0){
+                condition.await();
+            }
+            //干活
+            ++number;
+            System.out.println(Thread.currentThread().getName()+"生产了一个蛋糕！还剩"+number+"块蛋糕！");
+            //通知
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
-        //干活
-        --number;
-        System.out.println(Thread.currentThread().getName()+"消费了一块蛋糕！还剩"+number+"块蛋糕！");
-        //通知
-        this.notifyAll();
     }
-
-    public synchronized void productCake() throws InterruptedException {
-        //判断
-        while(number != 0){
-            this.wait();
+    public void consumerCake() throws InterruptedException {
+        lock.lock();
+        try {
+            //判断
+            while (number == 0){
+                condition.await();
+            }
+            //干活
+            --number;
+            System.out.println(Thread.currentThread().getName()+"消费了一块蛋糕！还剩"+number+"块蛋糕！");
+            //通知
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            condition.signalAll();
         }
-        //干活
-        ++number;
-        System.out.println(Thread.currentThread().getName()+"生产了一块蛋糕！还剩"+number+"块蛋糕！");
-        //通知
-        this.notifyAll();
     }
 }
 public class ProducerAndConsumerDemo {
     public static void main(String[] args) {
         Cake cake = new Cake();
         new Thread(() -> {
-            for (int i = 1; i <= 10 ; i++) {
+            for (int i = 0; i <= 10; i++) {
                 try {
-                    cake.consumCake();
+                    cake.productCake();
                     Thread.sleep(200);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } },"A").start();
+            }
+        },"A").start();
 
         new Thread(() -> {
-            for (int i = 1; i <= 10 ; i++) {
+            for (int i = 0; i <= 10; i++) {
                 try {
-                    cake.consumCake();
+                    cake.productCake();
                     Thread.sleep(200);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } },"B").start();
+            }
+        },"B").start();
 
         new Thread(() -> {
+            for (int i = 1; i <= 10 ; i++) {
+                try {
+                    cake.consumerCake();
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } },"C").start();
+
+        new Thread(() -> {
+            for (int i = 1; i <= 10 ; i++) {
+                try {
+                    cake.consumerCake();
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } },"D").start();
+
+        /*new Thread(() -> {
             for (int i = 1; i <= 10 ; i++) {
                 try {
                     cake.productCake();
@@ -70,6 +138,6 @@ public class ProducerAndConsumerDemo {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } },"D").start();
+            } },"D").start();*/
     }
 }
